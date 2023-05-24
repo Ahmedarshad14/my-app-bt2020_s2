2a50ecbdb3b8dcfd6fd4574c32554253c6050c04
Ahmedarshad14 <meahmed1@hotmail.com>
2023-05-25 04:26:58 +0500

    Updates app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Go home or
+          Go home <br>or
         </p>
         <a
           className="App-link"
